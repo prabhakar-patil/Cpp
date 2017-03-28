11e8bd818bee7f1a46410f5587310badbfea2457
Prabhakar Patil <prpatil12@gmail.com>
2017-03-28 21:49:38 +0530

    data structure namespace

---
+++ ds_namespace/list_client1.h
@@ -0,0 +1,76 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+#include <iostream>
+#include <stdexcept>
+
+namespace list
+{
+/************* Doubly *********************/
+	class dnode;
+	class dlist
+	{
+	public:
+		dlist();
+		~dlist ();
+		
+        	virtual void insert_beg (int n_data);
+	        virtual void insert_end (int n_data);
+        	virtual void insert_after_data (int e_data, int n_data);
+	        virtual void insert_before_data (int e_data, int n_data);
+        	virtual void del_beg (void);
+	        virtual void del_end (void);
+	        virtual void del_data (int e_data);
+	        virtual bool is_empty (void);
+        	virtual int length (void);
+	        virtual void display (void);
+		virtual bool search (int s_data);
+	protected:
+		dnode *head;
+
+		dnode *get_node (int data);
+		void g_insert (dnode *beg, dnode *mid, dnode *end);
+		void g_delete (dnode *del_node);
+		dnode *search_node (int s_data);
+	};
+
+	class dclist
+	{
+	public:
+		dclist();
+		~dclist ();
+		
+        	virtual void insert_beg (int n_data);
+	        virtual void insert_end (int n_data);
+        	virtual void insert_after_data (int e_data, int n_data);
+	        virtual void insert_before_data (int e_data, int n_data);
+        	virtual void del_beg (void);
+	        virtual void del_end (void);
+	        virtual void del_data (int e_data);
+	        virtual bool is_empty (void);
+        	virtual int length (void);
+	        virtual void display (void);
+		virtual bool search (int s_data);
+	protected:
+		dnode *head;
+
+		dnode *get_node (int data);
+		void g_insert (dnode *beg, dnode *mid, dnode *end);
+		void g_delete (dnode *del_node);
+		dnode *search_node (int s_data);
+	};
+
+/************ Singly ********************/
+	class snode
+	{
+	};
+
+	class slist
+	{
+	};
+	
+	class sclist
+	{
+	};
+};	
+
+#endif /*_LIST_H_*/
---
+++ ds_namespace/list_main2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "list_client2.h"
+#include "list_exception.h"
+
+using namespace list;
+
+int main (void)
+{
+	slist *ptr = new slist ();
+	
+	try {
+		ptr->del_beg ();
+	}
+	catch (list_empty &ref)
+	{
+		std::cout << ref.what () << std::endl;		
+	}
+
+	for (int cnt=0; cnt < 5; cnt++)
+	{
+		ptr->insert_beg (cnt);
+	}
+	ptr->display ();
+	
+	for (int cnt=5; cnt < 10; cnt++)
+	{
+		ptr->insert_end (cnt);
+	}
+	ptr->display ();
+	
+	ptr->insert_after_data(0, 100);
+	ptr->display ();
+	ptr->insert_before_data (0, 200);
+	ptr->display ();
+
+	ptr->del_beg ();
+	ptr->display ();
+	ptr->del_end ();
+	ptr->display ();
+	ptr->del_data (0);
+	ptr->display ();
+	
+	std::cout << "length: " << ptr->length () << std::endl;	
+	
+	if (ptr->search (200) == true)
+		std::cout << "search(200): present" << std::endl;
+	if (ptr->search (1000) == false)
+		std::cout << "search (1000): Absent" << std::endl;
+
+	delete (ptr);
+
+	return 0;	
+}
